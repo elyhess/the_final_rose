aaf94c56b6c2b1853e8a09f33ebc484a791df61a
Ely Hess <Ely.hess@me.com>
2021-01-02 16:58:52 -0700

    Add AR associations w/ specs

--- spec/models/outing_spec.rb
+++ spec/models/outing_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Contestant, type: :model do
+RSpec.describe Outing, type: :model do
   describe 'validations' do
     # it { should validate_presence_of :}
   end
 
   describe 'relationships' do
-    it {should belong_to :bachelorette }
-    it {should have_many :contestant_outings }
-    it {should have_many :outings }
+    it {should have_many :contestant_outings}
+    it {should have_many :contestants }
   end
 
   describe 'instance methods' do
