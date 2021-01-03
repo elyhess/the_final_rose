71096ca158401a9a0c5fd005d5d50efab85497de
Ely Hess <Ely.hess@me.com>
2021-01-02 17:34:54 -0700

    Add description to bachelorettes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_234809) do
+ActiveRecord::Schema.define(version: 2021_01_03_000928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_02_234809) do
     t.integer "season_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "contestant_outings", force: :cascade do |t|
